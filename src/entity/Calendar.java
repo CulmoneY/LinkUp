d57e3a3d9e3d109500f0532adc2278193e623b50
CulmoneY <yculmone@rogers.com>
2024-11-04 21:39:16 -0500

    started adding Calendar

--- src/entity/Calendar.java
+++ src/entity/Calendar.java
@@ -1,4 +1,39 @@
 package entity;
 
-public class Calendar {
+import java.util.List;
+
+public interface Calendar {
+    /**
+     * Returns the name of the calendar.
+     * @return the name of the calendar.
+     */
+    String getName();
+
+    void setName(String name);
+
+    /**
+     * Returns the events in the calendar.
+     * @return the events in the calendar.
+     */
+    List<Event> getEvents();
+
+    void setEvents(List<Event> events);
+
+    /**
+     * Adds an event to the calendar.
+     * @param event the event to add.
+     */
+    void addEvent(Event event);
+
+    /**
+     * Removes an event from the calendar.
+     * @param event the event to remove.
+     */
+    void removeEvent(Event event);
+
+    /**
+     * Returns the owner of the calendar.
+     * @return the owner of the calendar.
+     */
+    User getOwner();
 }
