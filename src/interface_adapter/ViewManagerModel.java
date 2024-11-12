af1f137ba3cf35a0222e57fd598750e4ac086c9a
neylnasr <misterneyl18@gmail.com>
2024-11-11 19:32:09 -0500

    View Manager Model updated for Login View

--- src/interface_adapter/ViewManagerModel.java
+++ src/interface_adapter/ViewManagerModel.java
@@ -1,14 +1,15 @@
 package interface_adapter;
 
+import java.beans.PropertyChangeSupport;
+
 /**
  * Model for the View Manager. Its state is the name of the View which
  * is currently active. An initial state of "" is used.
  */
 public class ViewManagerModel extends ViewModel<String> {
-
+    private String activeView;
     public ViewManagerModel() {
         super("view manager");
         this.setState("");
     }
-
 }
