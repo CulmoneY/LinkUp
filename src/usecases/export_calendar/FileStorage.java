41afda2b8e61f7e4c86c01e94ae47f275ba744f7
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 18:36:31 -0500

    filestorage

--- src/usecases/export_calendar/FileStorage.java
+++ src/usecases/export_calendar/FileStorage.java
@@ -5,8 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileStorage {
-    public static String saveToFile(String content, String fileName) throws IOException {
-        String filePath = "exports/" + fileName;
+    public static String saveToFile(String content, String name) throws IOException {
+        String filePath = "exports/" + name;
         Files.write(Paths.get(filePath), content.getBytes());
         return filePath;
     }
