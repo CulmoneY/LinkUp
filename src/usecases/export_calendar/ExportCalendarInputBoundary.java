8738787bba42e1f7c455f8d88d62e26a73d23bc2
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 18:24:01 -0500

    inputboundary

--- src/usecases/export_calendar/ExportCalendarInputBoundary.java
+++ src/usecases/export_calendar/ExportCalendarInputBoundary.java
@@ -1,7 +1,9 @@
 package usecases.export_calendar;
 
+import java.io.IOException;
+
 public interface ExportCalendarInputBoundary {
 
-    void execute(ExportCalendarInputData inputData);
+    void execute(ExportCalendarInputData inputData) throws IOException;
 
 }
