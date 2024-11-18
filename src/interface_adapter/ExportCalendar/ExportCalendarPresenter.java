06470032c96437b87121ded35445098946fde252
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 18:57:23 -0500

    presenter

--- src/interface_adapter/ExportCalendar/ExportCalendarPresenter.java
+++ src/interface_adapter/ExportCalendar/ExportCalendarPresenter.java
@@ -1,4 +1,26 @@
 package interface_adapter.ExportCalendar;
 
-public class ExportCalendarPresenter {
+import interface_adapter.AccountCreation.AccountCreationViewModel;
+import usecases.export_calendar.ExportCalendarOutputBoundary;
+import interface_adapter.ViewManagerModel;
+import usecases.export_calendar.ExportCalendarOutputData;
+
+public class ExportCalendarPresenter implements ExportCalendarOutputBoundary {
+    private final ExportCalendarViewModel exportCalendarViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public ExportCalendarPresenter(ExportCalendarViewModel exportCalendarViewModel, ViewManagerModel viewManagerModel) {
+        this.exportCalendarViewModel = exportCalendarViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void exportSuccess(ExportCalendarOutputData calendar) {
+
+    }
+
+    @Override
+    public void exportFail(String error) {
+
+    }
 }
