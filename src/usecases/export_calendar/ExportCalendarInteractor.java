1cfd7acba95fe4e43483f40d12dd8715b1c22f60
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 18:18:32 -0500

    interactor

--- src/usecases/export_calendar/ExportCalendarInteractor.java
+++ src/usecases/export_calendar/ExportCalendarInteractor.java
@@ -1,4 +1,21 @@
 package usecases.export_calendar;
 
-public class ExportCalendarInteractor {
+import entity.Calendar;
+import entity.User;
+
+public class ExportCalendarInteractor implements ExportCalendarInputBoundary {
+    final ExportCalendarOutputBoundary outputBoundary;
+
+    public ExportCalendarInteractor(ExportCalendarOutputBoundary outputBoundary) {
+        this.outputBoundary = outputBoundary;
+    }
+
+    @Override
+    public void execute(ExportCalendarInputData inputData) {
+        User user = inputData.getUser();
+        Calendar calendar = user.getUserCalendar();
+
+
+
+    }
 }
