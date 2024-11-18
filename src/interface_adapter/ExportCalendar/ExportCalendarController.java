8a8c883c55c5cb4bb22c891265d8e5f1467e9370
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 18:52:29 -0500

    controller

--- src/interface_adapter/ExportCalendar/ExportCalendarController.java
+++ src/interface_adapter/ExportCalendar/ExportCalendarController.java
@@ -1,4 +1,20 @@
 package interface_adapter.ExportCalendar;
 
+import entity.User;
+import usecases.export_calendar.ExportCalendarInputBoundary;
+import usecases.export_calendar.ExportCalendarInputData;
+
+import java.io.IOException;
+
 public class ExportCalendarController {
+    private final ExportCalendarInputBoundary exportCalendarInteractor;
+
+    public ExportCalendarController(ExportCalendarInputBoundary exportCalendarInteractor) {
+        this.exportCalendarInteractor = exportCalendarInteractor;
+    }
+
+    public void execute(User user) throws IOException {
+        ExportCalendarInputData inputData = new ExportCalendarInputData(user);
+        exportCalendarInteractor.execute(inputData);
+    }
 }
