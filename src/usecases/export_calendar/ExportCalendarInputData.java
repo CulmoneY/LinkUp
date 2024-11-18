82ad01911f6cc01d10eb94d79fc185ff24e60e88
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 17:52:37 -0500

    input data

--- src/usecases/export_calendar/ExportCalendarInputData.java
+++ src/usecases/export_calendar/ExportCalendarInputData.java
@@ -1,5 +1,14 @@
 package usecases.export_calendar;
 
+import entity.User;
+
 public class ExportCalendarInputData {
+    private User user;
+
+    public ExportCalendarInputData(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {return this.user;}
 
 }
