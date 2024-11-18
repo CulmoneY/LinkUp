f2d49a54a052f81d2636fd54ad074a65c0be24ca
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 17:56:14 -0500

    output data

--- src/usecases/export_calendar/ExportCalendarOutputData.java
+++ src/usecases/export_calendar/ExportCalendarOutputData.java
@@ -1,4 +1,11 @@
 package usecases.export_calendar;
 
 public class ExportCalendarOutputData {
+    private boolean success;
+
+    public ExportCalendarOutputData(boolean success) {
+        this.success = success;
+    }
+
+    public boolean getSuccess() {return this.success;}
 }
