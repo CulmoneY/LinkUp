d6a49ffb0cca3a1c32cc7c3038b51b800cb3d688
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 18:31:31 -0500

    interactor

--- src/usecases/export_calendar/ExportCalendarInteractor.java
+++ src/usecases/export_calendar/ExportCalendarInteractor.java
@@ -3,6 +3,8 @@ package usecases.export_calendar;
 import entity.Calendar;
 import entity.User;
 
+import java.io.IOException;
+
 public class ExportCalendarInteractor implements ExportCalendarInputBoundary {
     final ExportCalendarOutputBoundary outputBoundary;
 
@@ -11,11 +13,20 @@ public class ExportCalendarInteractor implements ExportCalendarInputBoundary {
     }
 
     @Override
-    public void execute(ExportCalendarInputData inputData) {
+    public void execute(ExportCalendarInputData inputData) throws IOException {
         User user = inputData.getUser();
         Calendar calendar = user.getUserCalendar();
 
+        if (calendar.getEvents().size() == 0) {
+            outputBoundary.exportFail("No events to export.");
+            return;
+        }
 
+        String icsContent = IcsFormatter.format(calendar);
+        String filePath = FileStorage.saveToFile(icsContent, user.getName() + "_calendar.ics");
+
+        outputBoundary.exportSuccess(new
+                ExportCalendarOutputData(true, filePath, "Export successful."));
 
     }
 }
