3d6245783f8d6476c9bcb8a35d85c4dc37e7d045
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-19 16:26:48 -0500

    output boundary

--- src/usecases/export_calendar/ExportCalendarOutputBoundary.java
+++ src/usecases/export_calendar/ExportCalendarOutputBoundary.java
@@ -1,7 +1,5 @@
 package usecases.export_calendar;
 
-import usecases.account_creation.AccountCreationOutputData;
-
 public interface ExportCalendarOutputBoundary {
 
     void exportSuccess(ExportCalendarOutputData calendar);
