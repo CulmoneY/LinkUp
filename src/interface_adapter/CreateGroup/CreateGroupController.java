aa509ea56ea4c5152f792ad4896fd5addbf9f647
neylnasr <misterneyl18@gmail.com>
2024-11-18 20:54:11 -0500

    Updates the CreateGroupController for the Create Group View.

--- src/interface_adapter/CreateGroup/CreateGroupController.java
+++ src/interface_adapter/CreateGroup/CreateGroupController.java
@@ -1,4 +1,25 @@
 package interface_adapter.CreateGroup;
 
+// have the user in Input Data
+
+import entity.User;
+import usecases.create_group.CreateGroupInputBoundary;
+import usecases.create_group.CreateGroupInputData;
+
+import java.util.List;
+
 public class CreateGroupController {
+    private CreateGroupInputBoundary inputBoundary;
+
+    public CreateGroupController(CreateGroupInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public void execute(String groupName, List<String> groupMembers, String current_user) {
+        CreateGroupInputData inputData = new CreateGroupInputData(groupName, groupMembers, current_user);
+        inputBoundary.execute(inputData);
+
+    }
+
+
 }
