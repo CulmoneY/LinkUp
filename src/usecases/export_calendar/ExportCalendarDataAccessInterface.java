9148d1459cbcb423fbf49b656389d348bf1199bb
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-19 16:38:46 -0500

    data access interface

--- src/usecases/export_calendar/ExportCalendarDataAccessInterface.java
+++ src/usecases/export_calendar/ExportCalendarDataAccessInterface.java
@@ -1,4 +1,11 @@
 package usecases.export_calendar;
 
+import entity.Calendar;
+
+import java.io.IOException;
+
 public interface ExportCalendarDataAccessInterface {
+
+    void saveData(Calendar calendar, String content) throws IOException;
+
 }
