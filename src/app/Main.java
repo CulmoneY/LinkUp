123f62965fc69dcfbe26be59413df75c69b67a9f
neylnasr <misterneyl18@gmail.com>
2024-11-18 20:52:14 -0500

    Updates Main for the Create Group View.

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         // first added view becomes the default view
-        final JFrame application = appBuilder.addAccountCreationView()
+        final JFrame application = appBuilder
+                .addAccountCreationView()
                 .addAccountCreationUseCase()
                 .addLoginView()
                 .addLoginUseCase()
                 .addGroupChatView()
                 .addUserSettingsView()
+                .addCreateGroupView()
                 .build();
 
 
