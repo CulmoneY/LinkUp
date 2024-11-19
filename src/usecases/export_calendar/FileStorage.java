79f41f2167927a4f680a1d91f58245466dd6e875
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-19 18:50:53 -0500

    file storage

--- src/usecases/export_calendar/FileStorage.java
+++ src/usecases/export_calendar/FileStorage.java
@@ -2,12 +2,21 @@ package usecases.export_calendar;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileStorage {
-    public static String saveToFile(String icsCalendar, String name) throws IOException {
-        String filePath = "exports/" + name;
-        Files.write(Paths.get(filePath), icsCalendar.getBytes());
+    public static String saveToFile(String calendar, String name) throws IOException {
+
+        String downloadsPath = System.getProperty("user.home") + "/Downloads/";
+        Path path = Paths.get(downloadsPath);
+
+        if (!Files.exists(path)) {
+            Files.createDirectories(path);
+        }
+
+        String filePath = path + name;
+        Files.write(Paths.get(filePath), calendar.getBytes());
         return filePath;
     }
 }
