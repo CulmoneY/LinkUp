453ffbf859b9aee9db23a2dce066cb2f58244d17
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-18 19:04:30 -0500

    state

--- src/interface_adapter/ExportCalendar/ExportCalendarState.java
+++ src/interface_adapter/ExportCalendar/ExportCalendarState.java
@@ -1,4 +1,19 @@
 package interface_adapter.ExportCalendar;
 
 public class ExportCalendarState {
+    private String filePath;
+    private String message;
+    private boolean success;
+
+    public String getFilePath() {return filePath;}
+
+    public void setFilePath(String filePath) {this.filePath = filePath;}
+
+    public String getMessage() {return message;}
+
+    public void setMessage(String message) {this.message = message;}
+
+    public boolean isSuccess() {return success;}
+
+    public void setSuccess(boolean success) {this.success = success;}
 }
