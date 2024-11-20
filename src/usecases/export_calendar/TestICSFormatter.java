b55f578499921fa9f82ab5abbfc25567feffa021
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-20 16:50:45 -0500

    unit test

--- src/usecases/export_calendar/TestICSFormatter.java
+++ src/usecases/export_calendar/TestICSFormatter.java
@@ -7,7 +7,6 @@ import entity.Event;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
