7e85479f9fd2e44810bb936407b897f20eac1ab1
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-19 21:01:17 -0500

    state

--- src/interface_adapter/ExportCalendar/ExportCalendarViewModel.java
+++ src/interface_adapter/ExportCalendar/ExportCalendarViewModel.java
@@ -3,8 +3,10 @@ package interface_adapter.ExportCalendar;
 import interface_adapter.ViewModel;
 
 public class ExportCalendarViewModel extends ViewModel<ExportCalendarState> {
+
     public ExportCalendarViewModel() {
         super("exportCalendarView");
         setState(new ExportCalendarState());
+
     }
 }
