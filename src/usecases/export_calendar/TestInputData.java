f2c648daa736af985e592075b158daef7a3f474d
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-20 20:05:31 -0500

    inputdata

--- src/usecases/export_calendar/TestInputData.java
+++ src/usecases/export_calendar/TestInputData.java
@@ -1,4 +1,30 @@
 package usecases.export_calendar;
 
+import entity.CommonCalendarFactory;
+import entity.CommonUserFactory;
+import entity.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestInputData {
+
+    @Test
+    public void testInputDataConstruction() {
+        CommonCalendarFactory calendarFactory = new CommonCalendarFactory();
+        CommonUserFactory userFactory = new CommonUserFactory();
+
+
+        Calendar calendar = calendarFactory.create("Test Calendar");
+        User user = userFactory.create("Test User", "Test Password", "English");
+        user.setUserCalendar(calendar);
+
+        ExportCalendarInputData inputData = new ExportCalendarInputData(user);
+
+        assertNotNull(inputData, "ExportCalendarInputData instance should not be null.");
+        assertEquals(user, inputData.getUser(), "User in input data should match the one provided during construction.");
+    }
 }
