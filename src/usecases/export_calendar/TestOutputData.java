825d0f71134cf430c4c70c9846b1356bf2e07f10
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-20 20:06:00 -0500

    outputdata

--- src/usecases/export_calendar/TestOutputData.java
+++ src/usecases/export_calendar/TestOutputData.java
@@ -1,4 +1,18 @@
 package usecases.export_calendar;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestOutputData {
+
+    @Test
+    public void testOutputData() {
+        String filePath = "/Downloads/test.ics";
+        ExportCalendarOutputData outputData = new
+                ExportCalendarOutputData(true, filePath, "Export successful.");
+
+        assertNotNull(outputData, "ExportCalendarOutputData instance should not be null.");
+        assertEquals(filePath, outputData.getFilePath(), "File path should match the one provided during construction.");
+    }
+
 }
