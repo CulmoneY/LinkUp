5a0585277a42b3e44f9f5a780b6c7ccda2e03984
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-21 20:00:27 -0500

    outputdata

--- src/usecases/export_calendar/TestOutputData.java
+++ src/usecases/export_calendar/TestOutputData.java
@@ -6,14 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestOutputData {
 
     @Test
-    public void testOutputData() {
+    public void testOutputDataWithValidFilePath() {
         String filePath = "/Downloads/test.ics";
         ExportCalendarOutputData outputData = new
                 ExportCalendarOutputData(true, filePath, "Export successful.");
 
         assertNotNull(outputData, "ExportCalendarOutputData instance should not be null.");
-        assertEquals(filePath, outputData.getFilePath(),
-                "File path should match.");
+        assertEquals(filePath, outputData.getFilePath(), "File path should match.");
+        assertTrue(outputData.getSuccess(), "Export should be successful.");
+        assertEquals("Export successful.", outputData.getMessage(), "Message should match.");
     }
 
+    @Test
+    public void testConstructorWithNullFilePath() {
+        ExportCalendarOutputData outputData = new
+                ExportCalendarOutputData(false, null, "Export fail.");
+
+        assertNotNull(outputData, "ExportCalendarOutputData instance should not be null.");
+        assertNull(outputData.getFilePath(),
+                "The file path should be null if null was provided during construction.");
+        assertFalse(outputData.getSuccess(), "Export should fail.");
+        assertEquals("Export fail.", outputData.getMessage(), "Message should match.");
+    }
 }
