301437aec7f20c0031df31782ff029fce5282054
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-22 00:48:30 -0500

    inputdata

--- src/usecases/export_calendar/ExportCalendarInputData.java
+++ src/usecases/export_calendar/ExportCalendarInputData.java
@@ -5,11 +5,24 @@ import entity.Group;
 
 public class ExportCalendarInputData {
     private User user;
+    private final Group group;
 
     public ExportCalendarInputData(User user) {
         this.user = user;
+        this.group = null;
     }
 
-    public User getUser() {return this.user;}
+    public ExportCalendarInputData(Group group) {
+        this.user = null;
+        this.group = group;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
 
 }
