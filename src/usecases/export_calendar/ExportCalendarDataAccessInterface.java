a7c5756588f3ea98de654e829423db911d4a6568
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-22 14:36:30 -0500

    dataaccessinterface

--- src/usecases/export_calendar/ExportCalendarDataAccessInterface.java
+++ src/usecases/export_calendar/ExportCalendarDataAccessInterface.java
@@ -2,11 +2,9 @@ package usecases.export_calendar;
 
 import entity.Calendar;
 
-import java.io.IOException;
-
 // add to MongoDAO
 public interface ExportCalendarDataAccessInterface {
 
-    void saveCalendar(Calendar calendar) throws IOException;
+    void saveCalendar(Calendar calendar);
 
 }
