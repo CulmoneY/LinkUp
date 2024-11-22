106bf68b9187622dd311ab29dc3ea315ae4d6b41
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-21 23:46:13 -0500

    inputdata

--- src/usecases/export_calendar/ExportCalendarInputData.java
+++ src/usecases/export_calendar/ExportCalendarInputData.java
@@ -3,17 +3,13 @@ package usecases.export_calendar;
 import entity.User;
 import entity.Group;
 
-// add Group (need for interactor as well, execute, and tests for both)
 public class ExportCalendarInputData {
     private User user;
-    //private Group group;
 
     public ExportCalendarInputData(User user) {
         this.user = user;
-        // this.group = group;
     }
 
     public User getUser() {return this.user;}
-    //public Group getGroup() {return this.group;}
 
 }
