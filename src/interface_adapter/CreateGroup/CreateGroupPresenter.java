581bcee37d3792930f7714999a0e2ffacab54e60
neylnasr <misterneyl18@gmail.com>
2024-11-23 23:00:18 -0500

    Commits the CreateGroup Presenter for the Create Group View.

--- src/interface_adapter/CreateGroup/CreateGroupPresenter.java
+++ src/interface_adapter/CreateGroup/CreateGroupPresenter.java
@@ -1,11 +1,26 @@
 package interface_adapter.CreateGroup;
 
+import interface_adapter.AccountCreation.AccountCreationViewModel;
+import interface_adapter.ViewManagerModel;
 import usecases.create_group.CreateGroupOutputBoundary;
 import usecases.create_group.CreateGroupOutputData;
 
 public class CreateGroupPresenter  implements CreateGroupOutputBoundary {
+
+    private final CreateGroupViewModel createGroupViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+
+    public CreateGroupPresenter(CreateGroupViewModel createGroupViewModel, ViewManagerModel viewManagerModel) {
+        this.createGroupViewModel = createGroupViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+
     @Override
     public void setPassView(CreateGroupOutputData group) {
+        CreateGroupState createGroupState = createGroupViewModel.getState();
+        createGroupViewModel.firePropertyChanged("createSuccess");
 
     }
 
