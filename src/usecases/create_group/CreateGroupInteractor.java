78e91f9e8d3ccfb1f168b0b3c0e8af9710933294
neylnasr <misterneyl18@gmail.com>
2024-11-23 23:02:35 -0500

    Create Group Interactor modified.

--- src/usecases/create_group/CreateGroupInteractor.java
+++ src/usecases/create_group/CreateGroupInteractor.java
@@ -26,7 +26,7 @@ public class CreateGroupInteractor implements CreateGroupInputBoundary {
     @Override
     public void execute(CreateGroupInputData inputData) {
        if (groupExist(inputData)) {
-           createGroupOutputBoundary.setFailView("group_exists");
+           createGroupOutputBoundary.setFailView("This Group Name Already Exists!");
        }
        else if (missingGroupName(inputData)) {
            createGroupOutputBoundary.setFailView("You Are Missing a Group Name!");
@@ -36,22 +36,20 @@ public class CreateGroupInteractor implements CreateGroupInputBoundary {
        else {
            List<String> groupMembersNames = inputData.getGroupMembers();
            List<User> groupMembers = createGroupDataAccessInterface.groupMembersToUsers(groupMembersNames);
+           groupMembers.add(inputData.getCurrent_user());
            Group group = groupFactory.create(inputData.getGroupName(), groupMembers);
            createGroupDataAccessInterface.saveGroup(group);
-           createGroupDataAccessInterface.addGroupToUser(inputData.getCurrent_user(), group);
+           createGroupDataAccessInterface.addGroupToUser(inputData.getCurrent_user().getName(), group);
+
            for (String groupmember : groupMembersNames) {
                createGroupDataAccessInterface.addGroupToUser(groupmember, group);
            }
+           inputData.getCurrent_user().addGroup(group);
            CreateGroupOutputData outputData = new CreateGroupOutputData(inputData.getGroupName(),
                    inputData.getGroupMembers(), true);
            createGroupOutputBoundary.setPassView(outputData);
 
        }
-    }
-
-    @Override
-    public void switchToMainView() {
-       //TODO: Implement this method.
     }
 
     public boolean groupExist(CreateGroupInputData inputData) {
@@ -60,6 +58,10 @@ public class CreateGroupInteractor implements CreateGroupInputBoundary {
 
     public boolean missingGroupName(CreateGroupInputData inputData) {
        return inputData.getGroupName() == null || inputData.getGroupName().isEmpty();
+    }
+
+    public boolean emptyGroupMembers(CreateGroupInputData inputData) {
+       return inputData.getGroupMembers() == null || inputData.getGroupMembers().isEmpty();
     }
 
 
