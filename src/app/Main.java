748db99d43322634ce457185ee01fede72667feb
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-25 18:49:02 -0500

    main

--- src/app/Main.java
+++ src/app/Main.java
@@ -14,6 +14,7 @@ public class Main {
                 .addChangeLanguageUseCase()
                 .addDeletePersonalEventUserCase()
                 .addCreateGroupUseCase()
+                .addExportCalendarUseCase()
                 .build();
 
         application.pack();
