c38a69aa05ac5795bb45cda968f9f1da8554f354
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-30 16:07:50 -0500

    delete input d

---
+++ src/usecases/delete_personal_event/DeleteGroupEventInputData.java
@@ -0,0 +1,33 @@
+package usecases.delete_personal_event;
+
+import entity.Group;
+
+public class DeleteGroupEventInputData {
+    Group group;
+    String eventName;
+    String startTime;
+    String endTime;
+
+    public DeleteGroupEventInputData(Group group, String eventName, String startTime, String endTime) {
+        this.group = group;
+        this.eventName = eventName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+}
