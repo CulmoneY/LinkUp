870aeb244ccaf58efb0fd90bf245c402599f328c
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-11-30 16:08:15 -0500

    delete output d

---
+++ src/usecases/delete_personal_event/DeleteGroupEventOutputData.java
@@ -0,0 +1,13 @@
+package usecases.delete_personal_event;
+
+public class DeleteGroupEventOutputData {
+    private String eventName;
+
+    public DeleteGroupEventOutputData(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return this.eventName;
+    }
+}
