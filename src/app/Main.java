a1f9e9f2b12e01c5837315d7c3b8233ac7ec549e
neylnasr <misterneyl18@gmail.com>
2024-11-30 22:48:17 -0500

    Updates main for the Remove Group Member use case.

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,6 +12,7 @@ public class Main {
                 .addPersonalEventUseCase()
                 .addFriendUseCase()
                 .addGroupMemberUseCase()
+                .addRemoveGroupMemberUseCase()
                 .addChangeLanguageUseCase()
                 .addDeletePersonalEventUserCase()
                 .addCreateGroupUseCase()
