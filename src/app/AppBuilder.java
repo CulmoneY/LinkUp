e87ff967057888d38b86469a26a6249e79f191b3
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-12-01 03:09:17 -0500

    appbuilder fix

--- src/app/AppBuilder.java
+++ src/app/AppBuilder.java
@@ -280,6 +280,7 @@ public class AppBuilder {
         application.setSize(1280, 720); // Fixed window size
         application.setLocationRelativeTo(null); // Center the window
         application.add(cardPanel);
+
         return application;
     }
 }
