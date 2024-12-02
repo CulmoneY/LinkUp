d1b07350c9f28ec6427601a344323b22cf59a4da
neylnasr <misterneyl18@gmail.com>
2024-12-02 01:56:08 -0500

    State for ModifyGroupName Usecase

---
+++ src/interface_adapter/ModifyGroupName/ModifyGroupNameState.java
@@ -0,0 +1,32 @@
+package interface_adapter.ModifyGroupName;
+
+public class ModifyGroupNameState {
+    private String oldGroupName;
+    private String newGroupName;
+    private String errorMessage;
+
+    public String getOldGroupName() {
+        return oldGroupName;
+    }
+
+    public void setOldGroupName(String oldGroupName) {
+        this.oldGroupName = oldGroupName;
+    }
+
+    public String getNewGroupName() {
+        return newGroupName;
+    }
+
+    public void setNewGroupName(String newGroupName) {
+        this.newGroupName = newGroupName;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
+
